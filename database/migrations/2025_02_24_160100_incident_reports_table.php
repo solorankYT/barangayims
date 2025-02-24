1942a2862bad3fbf657eb7b3ae37a0c5a88f90fe
Daniel Lozada <daniellozadamain@gmail.com>
2025-02-25 00:20:06 +0800

    Created First Version of Database

---
+++ database/migrations/2025_02_24_160100_incident_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incident_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('resident_id');
+            $table->string('title');
+            $table->string('incident_type');
+            $table->text('description');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+
+            $table->foreign('resident_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incident_reports');
+    }
+};
