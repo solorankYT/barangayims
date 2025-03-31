815029dd6c4919ef87c73dd11925dc5f17adbad6
solorankYT <43119617+solorankYT@users.noreply.github.com>
2025-03-31 23:03:35 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: 'resources/js/app.jsx', // Your entry point for React
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: 'public/build', // Ensure Vite outputs to the correct directory
+        emptyOutDir: true, // Clear the build folder before building
+    }
 });
